ef8b65bdfc7778bae746f93758ca0166b61c2b8d
Alex Baker <iamalexbaker@gmail.com>
2023-05-26 16:03:58 +0700

    Version 0.1.1

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,2 +1,5 @@
+### 0.1.1
+- Documented code.
+
 ### 0.1.0
 - Initial version.
--- lib/src/ready_mixin.dart
+++ lib/src/ready_mixin.dart
@@ -2,16 +2,21 @@ import 'dart:async';
 
 import 'package:ready_mixin/ready_mixin.dart';
 
-mixin ReadyManager {
+mixin ReadyMixin {
   ReadyState _readyState = ReadyState.initial;
 
   /// The current initialisation state.
   ReadyState get readyState => _readyState;
+
+  /// Override this to change the waiting time between checks in [ready].
   Duration get readyTimeout => const Duration(milliseconds: 50);
+
+  /// The error that caused the state to be [ReadyState.error].
   Object? readyError;
 
   /// Waits until the state is ready.
   /// Returns true on ready, and false on error.
+  /// If initialisation hasn't started yet, this will start it.
   /// Override [readyTimeout] to change the time between checks (default 50ms).
   Future<bool> get ready async {
     while (true) {
@@ -25,8 +30,14 @@ mixin ReadyManager {
     }
   }
 
+  /// Sets the state to [ReadyState.ready].
   void setReady() => setReadyState(ReadyState.ready);
+
+  /// Sets the state to [ReadyState.working].
   void setWorking() => setReadyState(ReadyState.working);
+
+  /// Sets the state to [ReadyState.error].
+  /// Optionally provide an [error], which will be stored in [readyError].
   void setError([Object? error]) {
     readyError = error;
     setReadyState(ReadyState.error);
@@ -36,10 +47,14 @@ mixin ReadyManager {
     _readyState = state;
   }
 
+  /// Call this to start initialisation.
+  /// Override [onInit] to provide your initialisation logic.
   void init() {
     _readyState = ReadyState.working;
     onInit();
   }
 
+  /// Override this with your initialisation code.
+  /// Make sure you end with [setReady] or [setError].
   void onInit();
 }
